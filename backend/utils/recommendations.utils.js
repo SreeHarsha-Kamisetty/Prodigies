b5fc577a0afa8f7bf08774bae564291058b18021
Geetesh Mehuria <70647591+geeteshmehuria@users.noreply.github.com>
2024-03-03 22:22:59 +0530

    Update recommendations.utils.js

--- backend/utils/recommendations.utils.js
+++ backend/utils/recommendations.utils.js
@@ -5,19 +5,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
 });
 
-<<<<<<< HEAD
-const recommendations = async (items) => {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "assistant",
-        content: `3 recipies with ${items}, just the recipie names all in one line seperated by commas`,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  });
-  let recipies = await chatCompletion.choices[0].message.content;
-=======
+
 const recommendations = async(items)=> {
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: 'assistant', content: `3 recipies with ${items}, just the recipie names all in one line seperated by commas . Always follow this format - item1,item2,item3 and avoid sending items with their number like 1.item1,2.item2` }],
@@ -29,12 +17,7 @@ const recommendations = async(items)=> {
     // console.log(recipies)
     return recipies;   
   }
->>>>>>> c38ebead584e9b0d4613f9844a4bfb3ca477a258
 
-  recipies = recipies.split(",");
-  // console.log(recipies)
-  return recipies;
-};
 
 module.exports = {
   recommendations,
