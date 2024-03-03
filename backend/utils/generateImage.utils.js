1feccf1746137ee17977d4de6fdce2b44bcc8b62
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-03-03 21:42:00 +0530

    generating real image according to recipe

--- backend/utils/generateImage.utils.js
+++ backend/utils/generateImage.utils.js
@@ -1,26 +1,28 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const generateImage = async(recipe) =>{
-    // const resp = await fetch('https://api.deepai.org/api/text2img', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'api-key': process.env.DEEPAI_API_KEY
-    //     },
-    //     body: JSON.stringify({
-    //         text: recipe,
-    //         grid_size: "1",
-    //         image_generator_version :"hd",
-    //         negative_prompt:"multiple images"
+    const resp = await fetch('https://api.deepai.org/api/text2img', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': process.env.DEEPAI_API_KEY
+        },
+        body: JSON.stringify({
+            text: recipe,
+            grid_size: "1",
+            image_generator_version :"hd",
+            negative_prompt:"blurry",
+            width:"1024",
+            height:"512"
             
-    //     }),
+        }),
        
-    // });
+    });
     
-    // const data = await resp.json();
-    let data = "https://www.vegrecipesofindia.com/wp-content/uploads/2020/01/paneer-butter-masala-1-1152x1536.jpg"
-    // console.log(data);
-    return data
+    const data = await resp.json();
+    // let data = "https://www.vegrecipesofindia.com/wp-content/uploads/2020/01/paneer-butter-masala-1-1152x1536.jpg"
+    console.log(data);
+    return data.output_url
 }
 
 module.exports={
